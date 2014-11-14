895a2faf4bbfde038362da6e6e636b5d586319ed
bingo2013 <bin.go2011@foxmail.com>
2014-11-14 12:29:58 +0800

    for injector

--- src/loader.js
+++ src/loader.js
@@ -8,16 +8,39 @@ function setupModuleLoader(window) {
 
 	var angular = ensure(window, 'angular', Object);
 
-	var createModule = function(name, requires) {
+	var createModule = function(name, requires, modules) {
+		if (name == 'hasOwnProperty') {
+			throw 'hasOwnProperty is not a valid module name';
+		}
+
 		var moduleInstance = {
-			name: name
+			name: name,
+			requires: requires,
+			constant: function(key, value) {
+				moduleInstance._invokeQueue.push(['constant', [key, value]]);
+			},
+			_invokeQueue: []
 		};
+		modules[name] = moduleInstance;
 		return moduleInstance;
 	};
 
+	var getModule = function(name, modules) {
+		if (modules.hasOwnProperty(name)) {
+			return modules[name];
+		} else {
+			throw 'Module ' +name+ ' is not available!';
+		}
+	}
+
 	ensure(angular, 'module', function() {
+		var modules = {};
 		return function(name, requires) {
-			return createModule(name, requires);
+			if (requires) {
+				return createModule(name, requires, modules);
+			} else {
+				return getModule(name, modules);
+			}
 		};
 	});
 }
