a14f61a0d64ebc59e6682feab8356ad5980041f6
bingo2013 <bin.go2011@foxmail.com>
2014-11-15 11:38:27 +0800

    injector done

--- src/injector.js
+++ src/injector.js
@@ -1,7 +1,11 @@
 'use strict';
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
 
 function createInjector(modulesToLoad) {
 	var cache = {};
+	var loadedModules = {};
 
 	var $provide = {
 		constant: function(key, value) {
@@ -12,13 +16,56 @@ function createInjector(modulesToLoad) {
 		}
 	};
 
-	_.forEach(modulesToLoad, function(moduleName) {
-		var module = angular.module(moduleName);
-		_.forEach(module._invokeQueue, function(invokeArgs) {
-			var method = invokeArgs[0];
-			var args = invokeArgs[1];
-			$provide[method].apply($provide, args);
+	function annotate(fn) {
+		if (_.isArray(fn)) {
+			return fn.slice(0, fn.length - 1);
+		} else if (fn.$inject) {
+			return fn.$inject;
+		} else if (!fn.length) {
+			return [];
+		} else {
+			var source = fn.toString().replace(STRIP_COMMENTS, '');
+			var argDeclaration = source.match(FN_ARGS);
+			return _.map(argDeclaration[1].split(','), function(argName) {
+				return argName.match(FN_ARG)[2];
+			});
+		}
+	};
+
+	function invoke(fn, self, locals) {
+		var args = _.map(annotate(fn), function(token) {
+			if (_.isString(token)) {
+				return locals && locals.hasOwnProperty(token) ?
+					locals[token] :
+					cache[token];
+			} else {
+				throw 'Incorrect injection token! Expected a string, got ' + token;
+			}
 		});
+		if (_.isArray(fn)) {
+			fn = _.last(fn);
+		}
+		return fn.apply(self, args);
+	}
+
+	function instantiate(Type, locals) {
+		var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
+		var instance = Object.create(UnwrappedType.prototype);
+		invoke(Type, instance, locals);
+		return instance;
+	}
+
+	_.forEach(modulesToLoad, function loadModule(moduleName) {
+		if (!loadedModules.hasOwnProperty(moduleName)) {
+			loadedModules[moduleName] = true;
+			var module = angular.module(moduleName);
+			_.forEach(module.requires, loadModule);
+			_.forEach(module._invokeQueue, function(invokeArgs) {
+				var method = invokeArgs[0];
+				var args = invokeArgs[1];
+				$provide[method].apply($provide, args);
+			});
+		}
 	});
 
 	return {
@@ -27,6 +74,9 @@ function createInjector(modulesToLoad) {
 		},
 		get: function(key) {
 			return cache[key];
-		}
+		},
+		annotate: annotate,
+		invoke: invoke,
+		instantiate: instantiate
 	};
 }
